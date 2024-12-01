b8d709128331c4afcd289e23f699659f1214e826
1143520 <2182802547@qq.com>
2024-12-01 22:27:14 +0800

    [BAT] 2024-12-01 22:27

--- js/main.js
+++ js/main.js
@@ -12,9 +12,6 @@ async function loadContents(showLoading = true) {
         
         if (!response.ok) {
             const data = await response.json();
-            if (response.status === 403) {
-                throw new Error('访问被拒绝：' + (data.message || '您没有访问权限'));
-            }
             throw new Error(data.details || data.error || '加载失败');
         }
         
