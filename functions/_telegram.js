a2ca8506bec05e94d77df61b9cabb9e82961bb73
1143520 <2182802547@qq.com>
2024-12-07 16:23:20 +0800

    [BAT] 2024-12-07 16:23

--- functions/_telegram.js
+++ functions/_telegram.js
@@ -6,6 +6,9 @@ async function sendToTelegram(env, message, parseMode = 'HTML') {
   }
 
   try {
+    // 确保消息不超过限制
+    const truncatedMessage = truncateMessage(message);
+
     const response = await fetch(`https://api.telegram.org/bot${env.TG_BOT_TOKEN}/sendMessage`, {
       method: 'POST',
       headers: {
@@ -13,7 +16,7 @@ async function sendToTelegram(env, message, parseMode = 'HTML') {
       },
       body: JSON.stringify({
         chat_id: env.TG_CHAT_ID,
-        text: message,
+        text: truncatedMessage,
         parse_mode: parseMode,
         disable_web_page_preview: false
       }),
@@ -38,7 +41,13 @@ function formatContentForTelegram(type, title, content, url = null) {
   message += `<b>标题:</b> ${escapeHtml(title)}\n`;
   
   if (type === 'text' || type === 'code' || type === 'poetry') {
-    message += `<b>内容:</b>\n${escapeHtml(content)}`;
+    message += `<b>内容:</b>\n`;
+    // 对于代码类型，使用代码格式
+    if (type === 'code') {
+      message += `<pre><code>${escapeHtml(content)}</code></pre>`;
+    } else {
+      message += escapeHtml(content);
+    }
   } else if (type === 'file' || type === 'image') {
     message += `<b>链接:</b> ${url}`;
   }
@@ -46,8 +55,35 @@ function formatContentForTelegram(type, title, content, url = null) {
   return message;
 }
 
+// 截断消息以符合 Telegram 限制
+function truncateMessage(message) {
+  const MAX_LENGTH = 4000; // 留一些余地给可能的格式化字符
+  
+  if (message.length <= MAX_LENGTH) {
+    return message;
+  }
+
+  // 检查是否包含代码块
+  const codeBlockMatch = message.match(/<pre><code>([\s\S]*?)<\/code><\/pre>/);
+  if (codeBlockMatch) {
+    const beforeCode = message.substring(0, codeBlockMatch.index);
+    const afterCode = message.substring(codeBlockMatch.index + codeBlockMatch[0].length);
+    const code = codeBlockMatch[1];
+    
+    // 如果代码太长，截断代码
+    if (code.length > MAX_LENGTH - 200) { // 预留200字符给其他内容
+      const truncatedCode = code.substring(0, MAX_LENGTH - 200) + '...(已截断)';
+      return beforeCode + '<pre><code>' + truncatedCode + '</code></pre>' + afterCode;
+    }
+  }
+
+  // 普通文本的截断
+  return message.substring(0, MAX_LENGTH - 3) + '...';
+}
+
 // HTML 转义
 function escapeHtml(text) {
+  if (!text) return '';
   return text
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
